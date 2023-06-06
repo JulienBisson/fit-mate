c39cd7943a85ba8b240057c1fb327a262184f6d0
Aubry Louvet <aubry.louvet@gmail.com>
2023-06-06 16:53:06 +0200

    search bar en cours de construction

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+eventexample = Event.create(title: "natation quai de la fosse", description: "seance de natation", localisation: "quai de la fosse, nantes", soldout: false, event_level: "beginnner", datetime: 2023-06-28)
