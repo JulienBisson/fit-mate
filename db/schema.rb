7828675aaa40d26e3ce7505fc61ea40579afec2a
Julien Bisson <130975079+JulienBisson@users.noreply.github.com>
2023-06-06 18:08:37 +0200

    add migration and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_06_151751) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,9 +69,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_151751) do
     t.bigint "organizer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "sport"
     t.float "latitude"
     t.float "longitude"
+    t.string "sport"
     t.index ["organizer_id"], name: "index_events_on_organizer_id"
   end
 
