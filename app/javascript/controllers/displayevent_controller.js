a58bb8f5ba7817c2f959b1f2de9202d643f7a44e
Julien Bisson <130975079+JulienBisson@users.noreply.github.com>
2023-06-09 11:50:00 +0200

    display card when click on marker

--- app/javascript/controllers/displayevent_controller.js
+++ app/javascript/controllers/displayevent_controller.js
@@ -1,13 +1,18 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="displayevent"
 export default class extends Controller {
-  static targets =  ['display']
+  static targets = ["display"];
   connect() {
     // console.log("Hello World!")
   }
 
   display(event) {
-    this.displayTargets.classList.add("d-none")
+  
+    const card = document.querySelector(
+      `#card_${parseInt(event.currentTarget.id)}`
+    );
+
+    card.classList.remove("d-none");
   }
 }
