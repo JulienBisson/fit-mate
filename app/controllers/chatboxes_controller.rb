21ed2ad27b21a7b0e882352443ccdeb08af2e2a9
Julien Bisson <130975079+JulienBisson@users.noreply.github.com>
2023-06-11 18:04:47 +0200

    realize front

--- app/controllers/chatboxes_controller.rb
+++ app/controllers/chatboxes_controller.rb
@@ -1,9 +1,9 @@
 class ChatboxesController < ApplicationController
 
   def show
-    @chatbox = Chatbox.find(params[:id])
     @event = Event.find(params[:event_id])
     @event.organizer = current_user
+    @chatbox = Chatbox.find_by(event_id: @event.id)
     @message = Message.new
   end
 end
