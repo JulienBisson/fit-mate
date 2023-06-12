1b67aa2fbb591a800a8dc9265050811ec1296707
Aubry Louvet <aubry.louvet@gmail.com>
2023-06-12 16:26:57 +0200

    travail en cours sur la table event_participant

--- app/controllers/event_participants_controller.rb
+++ app/controllers/event_participants_controller.rb
@@ -1,13 +1,24 @@
 class EventParticipantsController < ApplicationController
   def new
+    @event_participant = EventParticipant.new
   end
 
   def create
-  end
-
-  def edit
+    @event_participant = EventParticipant.new(event_participant_params)
+    @event_participant.user_id = current_user.id
+    if @event_participant.save!
+      redirect_to events_path
+    else
+      render :new
+    end
   end
 
   def update
+  end
+
+  private
+
+  def event_participant_params
+    params.require(:event_participant).permit(:event_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,14 @@
 Rails.application.routes.draw do
-  # get 'event_participants/new'
-  # get 'event_participants/create'
-  # get 'event_participants/edit'
-  # get 'event_participants/update'
-  # get 'events/index'
-  # get 'events/show'
-  # get 'events/new'
-  # get 'events/create'
   devise_for :users
   root to: "pages#home"
   namespace :organizer do
     resources :events, only: %i[new create edit update]
   end
   resources :events, only: %i[index show] do
-    resources :event_participants, only: %i[update]
+    resources :event_participants, only: %i[new create update]
     resources :chatboxes, only: %i[show] do
       resources :messages, only: %i[create]
     end
   end
   resources :users, only: %i[show]
 end
-# organizer quand je créé events mais user quand index/show
