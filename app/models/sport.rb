74e10d73a3f01112e32d155bfd1828e59f18e8a2
ALEXIA NICOLEAU <al.nicoleau27@gmail.com>
2023-06-12 14:36:17 +0200

    form-event-finished

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,5 +1,5 @@
 class Sport < ApplicationRecord
   LIST = ['soccer', 'football', 'basket', 'tennis', 'yoga', 'paddle', 'swimming',
           'bike', 'escalation', 'badminton', 'rugby', 'boxing', 'curling', 'judo',
-          'table tennis', 'fitness', 'volleyball', 'bodybuilding']
+          'fitness', 'volleyball', 'bodybuilding']
 end
