7780baeba2b21adf57e42d6af95408f48d8f12b4
Aubry Louvet <aubry.louvet@gmail.com>
2023-06-13 16:24:33 +0200

    ajout des méthodes interesse et participe ; itération grace à ses méthodes

--- app/controllers/event_participants_controller.rb
+++ app/controllers/event_participants_controller.rb
@@ -4,29 +4,40 @@ class EventParticipantsController < ApplicationController
   end
 
   def create
-    # @event_participant = EventParticipant.new
-    # @event_participant.user_id = current_user.id
-    # @event_participant.event_id = params[:event_id]
-    # @event_participant.status = params[:status]
     user_id = current_user.id
     event_id = params[:event_id]
     status = params[:status]
 
     if EventParticipant.exists?(user_id: user_id, event_id: event_id)
-      redirect_to events_path
+      @event_participant = EventParticipant.find_by(user_id: user_id, event_id: event_id)
+
+      if @event_participant.status == "intéressé"
+        @event_participant.status = "participe"
+        @event_participant.save!
+        redirect_to event_path(event_id)
+      else
+        redirect_to events_path
+      end
     else
       @event_participant = EventParticipant.new(user_id: user_id, event_id: event_id, status: status)
 
-      if @event_participant.save!
-        redirect_to event_path(@event_participant.event_id)
+      if @event_participant.save
+        redirect_to event_path(event_id)
       else
         render :new
       end
-
     end
   end
 
   def update
+    @event_participant = EventParticipant.new(user_id: user_id, event_id: event_id, status: status)
+    status = params[:status]
+
+    if @event_participant.update(status: status)
+      redirect_to event_path(@event_participant.event_id)
+    else
+      render plain: "ERREUR"
+    end
   end
 
   def destroy
