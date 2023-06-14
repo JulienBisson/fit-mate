a0c99f6a7df25004981e5d9c881a2abaf8f21f92
Julien Bisson <130975079+JulienBisson@users.noreply.github.com>
2023-06-14 11:54:41 +0200

    realize PWA

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.hosts << /\A[a-z\d-]+\.loca\.lt\z/ # add this line within the class
+
   # Enable server timing
   config.server_timing = true
 
