f894614314c2cc55a80a4fe7a0f90a0355482947
ALEXIA NICOLEAU <al.nicoleau27@gmail.com>
2023-06-14 12:19:19 +0200

    profil user finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+    @events = EventParticipant.where(user: @user, status: "participe")
+    @organizer = Event.where(organizer: @user)
   end
 end
